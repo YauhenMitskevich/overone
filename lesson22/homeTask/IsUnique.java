58823826bfe5be56fd3314b031282982fda07844
YauhenMitskevich <echm.em@gmail.com>
2021-11-11 00:11:41 +0300

    lesson20. HomeTask

--- lesson22/homeTask/IsUnique.java
+++ lesson22/homeTask/IsUnique.java
@@ -1,4 +1,20 @@
 package lesson22.homeTask;
 
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 public class IsUnique {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("Eug", "Mits");
+        map.put("Max", "Field");
+        map.put("Sam", "Cole");
+
+        Iterator<Map.Entry<String, String>> itr = map.entrySet().iterator();
+        while(itr.hasNext()) {
+            Map.Entry<String, String> entry =  itr.next();
+            String value = entry.getValue();
+        }
+    }
 }
