940b5e483fcf6488c64627a1d5eca6bde311fabf
YauhenMitskevich <echm.em@gmail.com>
2021-11-14 19:13:42 +0300

    hw24

--- lesson24/Main.java
+++ lesson24/Main.java
@@ -3,7 +3,10 @@ package lesson24;
 public class Main {
     public static void main(String[] args) {
         Fruit apple = new Fruit("Apple", 323);
+        Fruit lemon = new Fruit("Lemon", 35);
         Fruit.Bone AppleBone = apple.new Bone();
+        Fruit.Bone LemonBone = lemon.new Bone();
         AppleBone.getAmount();
+        LemonBone.getAmount();
     }
 }
